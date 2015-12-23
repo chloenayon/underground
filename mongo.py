045b3213d22169c06d801d57a438e7796339d7ea
Chloe Kaubisch <nemononiam@gmail.com>
2015-12-23 11:51:20 -0500

    geolocation works

--- mongo.py
+++ mongo.py
@@ -2,13 +2,29 @@ from pymongo import MongoClient
 
 conn = MongoClient()
 
-db = conn['thing']
-
 users = conn['users']
 places = conn['places']
 
 bobross = {'first':"Bob", 'last':"Ross", 'uname':"bobtheross", 'email':"bobtheross@bobross.com", 'pass':"happylittletrees"}
-stuy = {'lat':"", 'long':"", 'address':"", 'name':"school", 'description':"hell"}
+#first name
+#last name
+#username
+#email
+#password
 
-db.users.insert(bobross)
-db.places.insert(stuy)
+stuy = {'lat':"-70", 'long':"43", 'address':"here", 'name':"school", 'description':"hell"}
+#latitude
+#longitude
+#address
+#name/title
+#description
+
+#db.users.insert(bobross)
+#db.places.insert(stuy)
+
+def addPlace(lat, lon, add, title, desc):
+    conn = MongoClient()
+    db = conn['places']
+    db.places.insert({'lat': lat, 'long':lon, 'address': add, 'name':title, 'description':desc})
+
+
