2965e9d5b384c3b19abcd7081afd0021d5202fec
Chris Grant <chris@chrisgrant.co>
2016-01-24 15:48:49 -0800

    removed old stuff

--- app.py
+++ app.py
@@ -124,7 +124,7 @@ def edit_user(username):
 
 @app.route('/places/create', methods=["GET", "POST"])
 def create_place():
-    
+
     user = session['user']
     if user is None:
         return redirect(url_for('home'))
@@ -161,61 +161,6 @@ def create_place():
 def page_not_found(error):
     return render_template('404.html'), 404
 
-
-#
-# @app.route('/login', methods = ["GET", "POST"])
-# def login():
-#     if 'logged' not in session:
-#         session['logged'] = False
-#     if request.method == "GET":
-#         print session
-#         print session['logged']
-#         return render_template("login.html")
-#     else:
-#         u = request.form['uname']
-#         p = request.form['passwd']
-#         if (member_data.verify(u, p)):
-#             session['logged'] = True
-#             session['username'] = u
-#             return redirect(url_for('map'))
-#         else:
-#             return render_template("login.html")
-#
-# #To add a place:
-# # ** you can only add a place to the map of a specific member
-# #
-# #member_template.addPlace(request.form['uname'], request.form['lat'], request.form['lon'], request.form['title'], request.form['address'])
-# #
-# #
-# #
-# #
-# #
-# #
-#
-# @app.route('/signup', methods=["GET", "POST"])
-# def signup():
-#     if session['logged'] == False:
-#         return redirect(url_for('login'))
-#     else:
-#         if request.method == "GET":
-#             return render_template("signup.html")
-#         else:
-#             if (member_data.newMember(request.form['first'], request.form['last'], request.form['user'], request.form['mail'], request.form['pass'])):
-#                 return redirect(url_for('login'))
-#             else:
-#                 return render_template("signup.html")
-#
-# @app.route('/map', methods= ["GET", "POST"])
-# def map():
-#     if session['logged'] == False:
-#         return redirect(url_for('login'))
-#     else:
-#         if request.method == "GET":
-#             return render_template("mappage.html", name = session['username'])
-#         else:
-#             human = request.form["person"]
-#             list = member_data.getPlaces(human)
-#             return render_template("mappage.html", name = list)
 
 if __name__ == "__main__":
     app.debug = True
