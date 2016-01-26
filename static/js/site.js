432024c1bc78befe4dc2b4b77359424b076ce171
Chris Grant <chris@chrisgrant.co>
2016-01-25 20:08:52 -0800

    added place form autofill

--- static/js/site.js
+++ static/js/site.js
@@ -1,9 +1,10 @@
 MAPBOX_TOKEN = 'pk.eyJ1IjoiY2thdWJpc2NoIiwiYSI6ImNpaWJ2eGE2dzAxa3B3ZWx6NWYwZGx1dWIifQ.jSuKW32Avl_d3_TB2JqGlA';
 
 $(document).ready(function() {
-  $('select').select2();
-  $("a[data-target=\"#searchModal\"]").click(function(){
+  $('#addPlaceForm select').select2({placeholder: 'Choose an option…'});
+  $("a[data-target=\"#addPlaceModal\"]").click(function(){
     if (navigator.geolocation) {
+      $('#addPlaceModal .progress-circle-indeterminate').show()
       navigator.geolocation.getCurrentPosition(fillAddPlace);
     }
   })
@@ -11,10 +12,7 @@ $(document).ready(function() {
 });
 
 function fillAddPlace(position) {
-  // $('#addPlaceForm input[name="latitude"]').val(position.coords.latitude)
-  // $('#addPlaceForm input[name="longitude"]').val(position.coords.longitude)
-
-  $.getJSON('https://api.mapbox.com/geocoding/v5/mapbox.places/'+longitude+','+latitude+'.json?access_token='+MAPBOX_TOKEN, function(data) {
+  $.getJSON('https://api.mapbox.com/geocoding/v5/mapbox.places/'+position.coords.longitude+','+position.coords.latitude+'.json?access_token='+MAPBOX_TOKEN, function(data) {
     var place = data.features[0];
 
     if (place) {
@@ -22,8 +20,12 @@ function fillAddPlace(position) {
       $('#addPlaceForm input[name="latitude"]').val(place.center[0])
       $('#addPlaceForm input[name="longitude"]').val(place.center[1])
       $('#addPlaceForm input[name="address"]').val(place.place_name)
+      $('#addPlaceForm select').select2('val', place.properties.maki)
+      $('#addPlaceModal .progress-circle-indeterminate').hide()
     } else {
-
+      $('#addPlaceForm input[name="latitude"]').val(position.coords.latitude)
+      $('#addPlaceForm input[name="longitude"]').val(position.coords.longitude)
+      $('#addPlaceModal .progress-circle-indeterminate').hide()
     }
 
   })
